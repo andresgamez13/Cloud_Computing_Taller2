e9c316f5a46a68a8241faa3300284ad7928636db
Andres <gamezzfelipe98@gmail.com>
2021-08-02 11:25:09 -0500

    Script v1 del cliente de prueba

---
+++ TCPcliente.py
@@ -0,0 +1,25 @@
+from socket import *
+import time
+
+servidorNombre = "54.101.x.246" 
+servidorPuerto = 12000
+
+def request(ind,val):
+    Request = ['saldo','debitar {}'.format(val+50),'debitar {}'.format(val-20),'saldo','acreditar {}'.format(val*2),'saldo']
+    return Request[ind]
+
+sal=0
+for i in range(6):
+    clienteSocket = socket(AF_INET, SOCK_STREAM)
+    clienteSocket.connect((servidorNombre,servidorPuerto))
+    mensaje = request(i,sal)
+    print(mensaje)
+    clienteSocket.send(bytes(mensaje, "utf-8"))
+    if (mensaje=='saldo'):
+        sal = int(str(clienteSocket.recv(1024), "utf-8"))
+        print("Respuesta_S:\n"+str(sal))
+    else:
+        mensajeRespuesta= clienteSocket.recv(1024)
+        print("Respuesta:\n" + str(mensajeRespuesta, "utf-8"))
+    clienteSocket.close()
+    time.sleep(2)
